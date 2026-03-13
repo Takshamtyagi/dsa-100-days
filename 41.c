86fb6d987ef801927800b72656c2149abf9e1506
Takshamtyagi <taksham1618@gmail.com>
2026-03-13 19:40:39 +0530

    41.c

---
+++ 41.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Queue {
+    struct Node* front;
+    struct Node* rear;
+};
+
+struct Queue* createQueue() {
+    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+void enqueue(struct Queue* q, int value) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = value;
+    temp->next = NULL;
+
+    if (q->rear == NULL) {
+        q->front = q->rear = temp;
+        return;
+    }
+
+    q->rear->next = temp;
+    q->rear = temp;
+}
+
+int dequeue(struct Queue* q) {
+    if (q->front == NULL) {
+        printf("Queue Underflow\n");
+        return -1;
+    }
+
+    struct Node* temp = q->front;
+    int value = temp->data;
+    q->front = q->front->next;
+
+    if (q->front == NULL)
+        q->rear = NULL;
+
+    free(temp);
+    return value;
+}
