24cc0831c42dc8213c0cae60fe99ed3437517551
Takshamtyagi <taksham1618@gmail.com>
2026-03-20 12:22:25 +0530

    Implement function to count leaves in a binary tree

---
+++ 48.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int countLeaves(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        return 1;
+    }
+    int leftCount = countLeaves(root->left);
+    int rightCount = countLeaves(root->right);
+    return leftCount + rightCount;
+}
