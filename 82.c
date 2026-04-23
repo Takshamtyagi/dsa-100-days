7d08f850b20b115b37121b25b7be061e7fd47210
Takshamtyagi <taksham1618@gmail.com>
2026-04-23 09:57:33 +0530

    Create 82.c
    Implement Floyd-Warshall algorithm to find shortest paths in a graph.

---
+++ 82.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define INF 100000
+
+int main() {
+    int n;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    int dist[n][n];
+
+    // Input adjacency matrix
+    printf("Enter adjacency matrix (use %d for no edge):\n", INF);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+        }
+    }
+
+    // Floyd-Warshall Algorithm
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Output result
+    printf("\nShortest distance matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
